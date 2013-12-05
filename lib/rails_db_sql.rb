ac9a4780ee518212e8d3629e372b3e23dba3a40f
Tomasz Bak <t.bak@selleo.com>
2013-12-05 19:00:01 +0100

    add errors summary

--- lib/rails_db_sql.rb
+++ lib/rails_db_sql.rb
@@ -5,15 +5,22 @@ module RailsDbSql
   require "rails_db_sql/railtie" if defined?(Rails::Railtie)
 
   def self.reload(pattern="db/sql/**/*.sql")
+    errors = {}
     Dir.glob(pattern).sort.each do |path|
       File.open path, "r" do |file|
-        puts "RailsDbSql executing #{path}"
+        puts "RailsDbSql: executing #{path}"
         begin
          execute_sql file.read
         rescue => e
-          puts e
+          errors[path] = e.to_s
         end
       end
+    end
+    unless errors.empty?
+      errors.each_with_index do |(path,details),index|
+        puts "\n#{index+1}) Error in #{path}\n#{details}"
+      end
+      puts "\nRailsDbSql: #{errors.size} errors"
     end
   end
 
